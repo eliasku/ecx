570e4949f35ea6ff9a82a8ce21c5a4fcc1ca3de4
Ilya Kuzmichev <ilyak@playtika.com>
2018-05-16 01:26:26 +0300

    C++ unsafe cast for Haxe 4.0 (issue #10)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,6 @@
 # 0.1.1
 
-- Fix Haxe compatibility
+- Basic Haxe 4.0 support 
 - Remove `power-of-two` dependency
+- C++ unsafe cast for Haxe 4.0 (issue #10)
+- CI scripts updated
--- src/ecx/ds/Cast.hx
+++ src/ecx/ds/Cast.hx
@@ -5,22 +5,20 @@ package ecx.ds;
 @:dce
 class Cast {
 
-    // TODO: fix C++ dynamic_cast for the latest HXCPP
-
     @:extern inline public static function unsafe<TIn, TClass>(value:TIn, clazz:Class<TClass>):TClass {
-//        #if (cpp && haxe_ver >= 3.3)
-//        return cpp.Pointer.fromRaw(cpp.Pointer.addressOf(value).rawCast()).value;
-//        #else
+        #if (cpp && haxe_ver >= 3.3)
+        return (cpp.Pointer.fromRaw(cpp.Pointer.addressOf(value).rawCast()):cpp.Pointer<TClass>).value;
+        #else
         return cast value;
-//        #end
+        #end
     }
 
     @:unreflective // no using / generic ?
     @:extern inline public static function unsafe_T<TIn, TOut>(value:TIn):TOut {
-//        #if (cpp && haxe_ver >= 3.3)
-//        return cpp.Pointer.fromRaw(cpp.Pointer.addressOf(value).rawCast()).value;
-//        #else
+        #if (cpp && haxe_ver >= 3.3)
+        return (cpp.Pointer.fromRaw(cpp.Pointer.addressOf(value).rawCast()):cpp.Pointer<TOut>).value;
+        #else
         return cast value;
-//        #end
+        #end
     }
 }
